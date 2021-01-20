f45440928a9d31318d7d2c35703b894718c29471
Ruben Winant <r1998w@hotmail.com>
2021-01-20 14:32:22 +0100

    added return in fPassword

--- pages/forgotPassword.tsx
+++ pages/forgotPassword.tsx
@@ -1,32 +1,43 @@
+import { useRouter } from "next/router";
+import { FormEvent } from "react";
 import Layout from "../components/Layout";
 
 const ForgotPasswordPage = () => {
-  <Layout title="Lighthouse - Brand guide specialist">
-    <section
-      className="contentContainer"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <h1>Reset password?</h1>
-      <form>
-        <div className="formInputRow">
-          <label>Email</label>
-          <input
-            className="formInput"
-            type="email"
-            placeholder="Email"
-            required
-          />
-        </div>
-        <button className="nextButton" type="submit">
-          Reset
-        </button>
-      </form>
-    </section>
-  </Layout>;
+  const router = useRouter();
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault;
+    router.push("/login");
+  };
+
+  return (
+    <Layout title="Lighthouse - Brand guide specialist">
+      <section
+        className="contentContainer"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Reset password?</h1>
+        <form>
+          <div className="formInputRow">
+            <label>Email</label>
+            <input
+              className="formInput"
+              type="email"
+              placeholder="Email"
+              required
+            />
+          </div>
+          <button onClick={onSubmit} className="nextButton" type="submit">
+            Reset
+          </button>
+        </form>
+      </section>
+    </Layout>
+  );
 };
 
 export default ForgotPasswordPage;
